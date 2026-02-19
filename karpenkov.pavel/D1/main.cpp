c86a0ecf00a4aa3c12890d0f10787ea12059cf64
xRonnyn <pashuha.k@gmail.com>
2026-02-19 18:50:56 +0300

    reading letters

--- karpenkov.pavel/D1/main.cpp
+++ karpenkov.pavel/D1/main.cpp
@@ -1,5 +1,44 @@
 #include <iostream>
 
+namespace karpenkov{
+  char * expand (char * arr, size_t len, size_t newLen){
+    char * str = new char[len + newLen];
+    for (size_t i = 0; i < len; i++){
+      str[i] = arr[i];
+    }
+    delete[] arr;
+    arr = str;
+    return arr;
+  }
+  char * addNewLetters(char * arr, size_t size, size_t count, char letter){
+    for (size_t i = 0; i < count; i++){
+      arr[size++] = letter;
+    }
+    return arr;
+  }
+}
+
 int main()
 {
+  size_t size = 0, cap = 10, number = 0;
+  char letter;
+  char * r = new char[cap];
+  while (std::cin >> number){
+    std::cin >> letter;
+    if (size + number >= cap){
+      try{
+        r = karpenkov::expand(r, size, number);
+        cap += number;
+        r = karpenkov::addNewLetters(r, size, number, letter);
+      }
+      catch(std::bad_alloc){
+        std::cerr << "bad_alloc" << '\n';
+        return 2;
+      }
+    }
+    else{
+      r = karpenkov::addNewLetters(r, size, number, letter);
+    }
+    size += number;
+  }
 }
